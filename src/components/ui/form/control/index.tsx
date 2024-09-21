0ff2a86a174120747ceaa3ec79698a7d1d1d6da5
georgyguei <georgy.guei@efrei.net>
2024-09-21 23:37:42 +0200

    :recycle: refactor(components/ui/form): enhance FormControl component with memo and forwardRef
    - Refactor FormControl component to use memo and forwardRef for performance optimization
    - Maintain existing functionality and props including isInvalid, isRequired, isDisabled, and isReadOnly
    - Enhance styling and structure for better maintainability

--- src/components/ui/form/control/index.tsx
+++ src/components/ui/form/control/index.tsx
@@ -1,6 +1,7 @@
+import Box from '@/components/ui/layout/box';
+import type { UIComponent } from '@/components/ui/type';
 import { cn } from '@/lib/utils';
-import Box from '../../layout/box';
-import type { UIComponent } from '../../type';
+import { forwardRef, memo } from 'react';
 
 type FormControlProps = {
   isInvalid?: boolean;
@@ -20,18 +21,20 @@ type FormControlProps = {
  * @param props - The props of the FormControl component.
  * @returns The FormControl component.
  */
-const FormControl: UIComponent<'div', FormControlProps> = props => {
-  const { isInvalid, className, ...rest } = props;
+const FormControl = memo(
+  forwardRef(((props, ref) => {
+    const { isInvalid, className, ...rest } = props;
 
-  return (
-    <Box
-      className={cn('relative w-full', className)}
-      role="group"
-      data-invalid={isInvalid}
-      {...rest}
-    />
-  );
-};
+    return (
+      <Box
+        className={cn('relative w-full', className)}
+        role="group"
+        data-invalid={isInvalid}
+        {...rest}
+      />
+    );
+  }) satisfies UIComponent<'div', FormControlProps>)
+) as UIComponent<'div', FormControlProps>;
 
 FormControl.displayName = 'FormControl';
 
