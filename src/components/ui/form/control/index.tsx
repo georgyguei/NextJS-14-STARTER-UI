76bd5dd50eba72549a1b6ab620f28c3b8d43c477
georgyguei <georgy.guei@efrei.net>
2024-09-21 23:52:37 +0200

    :sparkles: feat(components/ui/form): enhance FormControl component with required indicator support
    - Add support for required indicators in FormControl component
    - Update FormControl to handle isRequired prop and apply appropriate indicators
    - Enhance styling and structure for better maintainability

--- src/components/ui/form/control/index.tsx
+++ src/components/ui/form/control/index.tsx
@@ -1,9 +1,16 @@
+import FormRequiredIndicator from '@/components/ui/form/control/indicator';
 import Box from '@/components/ui/layout/box';
 import type { UIComponent } from '@/components/ui/type';
 import { cn } from '@/lib/utils';
-import { forwardRef, memo } from 'react';
+import {
+  Children,
+  cloneElement,
+  forwardRef,
+  isValidElement,
+  memo,
+} from 'react';
 
-type FormControlProps = {
+export type FormControlProps = {
   isInvalid?: boolean;
   isRequired?: boolean;
   isDisabled?: boolean;
@@ -23,15 +30,45 @@ type FormControlProps = {
  */
 const FormControl = memo(
   forwardRef(((props, ref) => {
-    const { isInvalid, className, ...rest } = props;
+    const { isInvalid, isRequired, className, children, ...rest } = props;
+    const remainingProps: object = { ...rest, ref };
+
+    const formRequiredIndicatorElement = isRequired
+      ? Children.toArray(children).find(child => {
+          if (isValidElement(child)) {
+            // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+            return (child as any).type.displayName === 'FormRequiredIndicator';
+          }
+        }) || <FormRequiredIndicator>*</FormRequiredIndicator>
+      : undefined;
+
+    const formElements = isRequired
+      ? Children.map(children, child => {
+          if (isValidElement(child)) {
+            // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+            switch ((child as any).type.displayName) {
+              case 'FormLabel':
+                return cloneElement(child as React.ReactElement, {
+                  isRequired: isRequired,
+                  requiredIndicator: formRequiredIndicatorElement,
+                });
+              case 'FormRequiredIndicator':
+                return null;
+            }
+          }
+          return child;
+        })
+      : children;
 
     return (
       <Box
         className={cn('relative w-full', className)}
         role="group"
         data-invalid={isInvalid}
-        {...rest}
-      />
+        {...remainingProps}
+      >
+        {formElements}
+      </Box>
     );
   }) satisfies UIComponent<'div', FormControlProps>)
 ) as UIComponent<'div', FormControlProps>;
